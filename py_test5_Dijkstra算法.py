26c4d4b96ee015367d04fc56f69e7cc47a0797df
BalboooA <joer@vip.qq.com>
2018-02-18 22:58:47 +0800

    加入路径输出

--- py_test5_Dijkstra算法.py
+++ py_test5_Dijkstra算法.py
@@ -1,15 +1,16 @@
 #coding time: 18.Feb
 #给出一个无向图，实现Dijkstra算法
 #具体的路径输出还没写，之后补上
+#路径输入已补上 - 18.Feb
 '''
 eg.无向图
-            ①
+           【1】
       p=5 /    ╲ p=5
         /        ╲
-      ⓪ ———p=7—— ②
-          ╲       ︱ p=1
-           p=3╲   ︱
-      ④ ———p=2—— ③
+      【0】———p=7——【2】
+           ╲       ︱ p=1
+             p=3╲  ︱
+      【4】———p=2——【3】
 '''
 class UndirectedGraph:
 
@@ -36,27 +37,35 @@ class UndirectedGraph:
         s_flag[0] = 1
         #将起始点：0号点加入集合S
         distance = self.arc_matrix[0]
-        #distance储存起始点到其余各点的距离
-        #可改变[]中的值选择不同的起始点。
-        for j in range(s_flag.count(0)):
-            index = distance.index(min(distance))
-            #index为distance中距离起始点最短的端点编号
-            if s_flag[index] == 0:
-                s_flag[index] = 1
-            #将非S中距离最近的点加入集合S
+        # distance储存起始点到其余各点的距离。
+        # 可改变[]中的值选择不同的起始点。
+        path = ["V0"] * self.point_num
+        #利用path列表储存路径信息。
+        while(s_flag.count(0)):
+            dist_min = float("inf")
             for i in range(self.point_num):
-                if s_flag[i] == 0:
-                    #遍历非S中的点，在加入新点后改写其到0号点的距离
-                    if self.arc_matrix[i][index] + distance[index] <= distance[i]:
-                         distance[i] = self.arc_matrix[i][index] + distance[index]
-        return distance
+                if ( s_flag[i] == 0 and distance[i] < dist_min ):
+                    index = i
+            #index为非S集中距离起始点最短的端点编号
+            s_flag[index] = 1
+            #将该点加入至S集合中
+            path[index] += "->V" + str(index)
+            #加入S集合意味着从起点到达该点路径已结束，路径写入。
+            for i in range(self.point_num):
+                if (s_flag[i] == 0 and self.arc_matrix[i][index] + distance[index] <= distance[i]):
+                    distance[i] = self.arc_matrix[i][index] + distance[index]
+                    # 遍历非S中的点，在加入新点至S后，若距离变短，则改写其到起始点的距离
+                    path[i] += "->V" + str(index)
+                    # 向path列表中写入中间点路径。
+        return distance , path
 
 
 graph = UndirectedGraph(5,6)
 Arc_matrix = graph.arc_matrix_update()
 print("邻接矩阵为：" + str(Arc_matrix))
-Distance = graph.Dijkstra()
+Distance , Path = graph.Dijkstra()
 print("从0号点出发，到各点的最短距离为：" + str(Distance))
+print("从0号点出发，到各点的最短路径为：" + str(Path))
 
 #print(邻接矩阵为：[[inf, 5.0, 7.0, 3.0, inf],
 #                 [5.0, inf, 5.0, inf, inf],
@@ -64,3 +73,4 @@ print("从0号点出发，到各点的最短距离为：" + str(Distance))
 #                   [3.0, inf, 1.0, inf, 2.0],
 #                    [inf, inf, inf, 2.0, inf]])
 #print(从0号点出发，到各点的最短距离为：[inf, 5.0, 4.0, 3.0, 5.0])
+#print(从0号点出发，到各点的最短路径为：['V0', 'V0->V1', 'V0->V3->V2', 'V0->V3', 'V0->V3->V4'])
